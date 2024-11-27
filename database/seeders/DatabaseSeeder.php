c8d56624794169f690a8686ac0bee0d178af8736
RizkyGnw <rzkygnw@gmail.com>
2024-11-27 20:16:36 +0700

    Menambahkan seeder UserData,Student, Department

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            UserDataSeeder::class,
+            StudentSeeder::class,
+            DepartmentSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
