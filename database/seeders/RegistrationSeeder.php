92f092b2cdae25651d3989b05d7978e237b1fadc
ROYJRDY <pontianakcity55555@gmail.com>
2024-11-27 20:35:59 +0700

    Menambahkan seeder StudentDepartment, Registration, Payment

--- database/seeders/RegistrationSeeder.php
+++ database/seeders/RegistrationSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Registration;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use function Symfony\Component\Clock\now;
 
 class RegistrationSeeder extends Seeder
 {
@@ -12,6 +15,34 @@ class RegistrationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Registration::create([
+            'student_id' => 1,
+            'registration_date' => now(),
+            'status' => 'pending',
+        ]);
+
+        Registration::create([
+            'student_id' => 2,
+            'registration_date' => now(),
+            'status' => 'accepted',
+        ]);
+
+        Registration::create([
+            'student_id' => 3,
+            'registration_date' => now(),
+            'status' => 'accepted',
+        ]);
+
+        Registration::create([
+            'student_id' => 4,
+            'registration_date' => now(),
+            'status' => 'accepted',
+        ]);
+
+        Registration::create([
+            'student_id' => 5,
+            'registration_date' => now(),
+            'status' => 'accepted',
+        ]);
     }
 }
